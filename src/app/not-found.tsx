34abc9b528873a0dfecb9ce20ec11d3363efa3ea
Pawan Kumar <104499385+PawanKumar85@users.noreply.github.com>
2024-09-02 11:16:52 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ const Notfound = () => {
                         </svg>
                     </p>
                     <h1 className="mt-3 text-2xl font-semibold text-black  md:text-3xl">Page not found</h1>
-                    <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn't exist. Here are some helpful links:</p>
+                    <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesnt exist. Here are some helpful links:</p>
                     <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
 
                         <Link href={"/"}>
